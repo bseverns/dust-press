28a76454178a6101bbb61503abfe1dabcfc2f5cd
Ben Severns <severns3@gmail.com>
2025-12-03 13:05:11 -0600

    more juce

--- native/.juce-build/tools/cmake_install.cmake
+++ native/.juce-build/tools/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/local")
+  set(CMAKE_INSTALL_PREFIX "/Users/bseverns/Documents/GitHub/dust-press/native/.juce-kit")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
